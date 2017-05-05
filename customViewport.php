3c7c777640514b706af0c6c42ff571f74b599de6
Tamacun <lidelunli@gmail.com>
2017-05-04 18:54:47 -0700

    Rename Files
    Simplify File names and references

--- customViewport.php
+++ customViewport.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-<script src="customstrapped.js"></script>
+<script src="customViewport.js"></script>
 
 
-<link rel="stylesheet" type="text/css" href="customfixed.css">
+<link rel="stylesheet" type="text/css" href="customViewport.css">
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<title></title>
 </head>
